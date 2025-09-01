815db12ca0d4a37f5265adfabf1c37b2dfedb9d4
kumuthiniak <kumuthini.program@gmail.com>
2025-09-02 00:08:10 +0530

    ak

--- src/main/java/com/example/contact_management_system/ContactManagementSystemApplication.java
+++ src/main/java/com/example/contact_management_system/ContactManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactManagementSystemApplication {
-	// ijihtghrtighrtihrtihgirgrtighrighr
+	// ijihtghrtighrtihrtihgirgrtighrighrgugugugugugg
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactManagementSystemApplication.class, args);
